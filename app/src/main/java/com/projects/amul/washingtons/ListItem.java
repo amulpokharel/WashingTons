324604a5c5f920a2f86f23233669473113c6006f
Amul Pokharel <amulpokharel@users.noreply.github.com>
2017-05-09 11:28:34 -0400

    Fixed the layout for washerfragment, adjusted some styling, trying a font to replace the washing guide images

--- app/src/main/java/com/projects/amul/washingtons/ListItem.java
+++ app/src/main/java/com/projects/amul/washingtons/ListItem.java
@@ -1,8 +1,19 @@
 package com.projects.amul.washingtons;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by amulpok on 5/1/17.
  */
 
 public class ListItem {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View main =  inflater.inflate(R.layout.list_item, container, false);
+        //((TextView)main.findViewById(R.id.firstLine)).setTypeface();
+        return main;
+    }
 }
