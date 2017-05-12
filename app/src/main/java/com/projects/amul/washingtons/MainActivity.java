e151540c9e9efb6dfc755e4ef0b21342750ef8f5
Amul Pokharel <amulpokharel@users.noreply.github.com>
2017-05-12 15:34:19 -0400

    implemented ViewPager to enable swiping between fragments. @todo fix bottom nav bar to set active while swiping

--- app/src/main/java/com/projects/amul/washingtons/MainActivity.java
+++ app/src/main/java/com/projects/amul/washingtons/MainActivity.java
@@ -4,7 +4,8 @@ package com.projects.amul.washingtons;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,51 +13,88 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static class PagerAdapter extends FragmentPagerAdapter{
+        private static int NUM_ITEMS = 3;
+        public PagerAdapter (FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return WasherFragment.newInstance();
+                case 1:
+                    return DryerFragment.newInstance();
+                case 2:
+                    return OtherFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+    }
+
+    FragmentPagerAdapter viewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final ViewPager vPager = (ViewPager) findViewById(R.id.mainFrame);
+        viewPagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        vPager.setAdapter(viewPagerAdapter);
+
         final BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navbar);
 
-
         bottomNavigationView.getMenu().getItem(0).setChecked(true);
 
-        // define your fragments here
-        final Fragment washerFragment = new WasherFragment();
-        final Fragment dryerFragment = new DryerFragment();
-        final Fragment otherFragment = new OtherFragment();
+        vPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.mainFrame, washerFragment)
-                .commit();
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(position);
+            }
 
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                bottomNavigationView.getMenu().getItem(position);
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         // handle navigation selection
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    FragmentTransaction fragmentTransaction;
 
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.menu_washer:
                                 bottomNavigationView.getMenu().getItem(0);
-                                fragmentTransaction = fragmentManager.beginTransaction();
-                                fragmentTransaction.replace(R.id.mainFrame, washerFragment).commit();
+                                vPager.setCurrentItem(0);
                                 return true;
                             case R.id.menu_dryer:
                                 bottomNavigationView.getMenu().getItem(1);
-                                fragmentTransaction = fragmentManager.beginTransaction();
-                                fragmentTransaction.replace(R.id.mainFrame, dryerFragment).commit();
+                                vPager.setCurrentItem(1);
                                 return true;
                             case R.id.menu_home:
                                 bottomNavigationView.getMenu().getItem(2);
-                                fragmentTransaction = fragmentManager.beginTransaction();
-                                fragmentTransaction.replace(R.id.mainFrame, otherFragment).commit();
+                                vPager.setCurrentItem(2);
                                 return true;
                         }
                         return false;
